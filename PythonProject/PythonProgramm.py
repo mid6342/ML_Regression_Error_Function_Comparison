96e98a6b307bb342af3a785215dc0a46ce120884
Michael Dollinger <michi.dollinger96@web.de>
2021-11-03 11:18:23 +0100

    Implemented print for train and ideal table

--- PythonProject/PythonProgramm.py
+++ PythonProject/PythonProgramm.py
@@ -2,15 +2,31 @@ import sqlite3
 import csv
 import pandas
 import sqlalchemy as db
+import numpy as np
+
 
 conn = sqlite3.connect("FindingFunctions.db")
-
 cur = conn.cursor()
 
+# print ideal table
+ideal_table = cur.execute("SELECT * FROM ideal")
+for i in ideal_table:
+    print(i)
 
-cur.execute("SELECT x FROM train")
-result=cur.fetchall()
-print(result)
+print("-------------------------------------------------------------------------------------------")
+print("-------------------------------------------------------------------------------------------")
+print("-------------------------------------------------------------------------------------------")
+print("-------------------------------------------------------------------------------------------")
+
+# print train table
+train_table = cur.execute("SELECT * FROM train")
+for k in train_table:
+    print(k)
+
+
+
+#result=cur.fetchall()
+#print(result)
 #result = conn.execute("SELECT x, y1 FROM train")
 #print(result)
 
