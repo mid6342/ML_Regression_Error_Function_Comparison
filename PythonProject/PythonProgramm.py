f7d285b03597caae8e000bddf4611a32826f7846
Michael Dollinger <michi.dollinger96@web.de>
2021-11-11 19:43:34 +0100

    Implemented criterion 1 to get 4 ideal functions and calculated the max deviation for one of the 4 ideal functions

--- PythonProject/PythonProgramm.py
+++ PythonProject/PythonProgramm.py
@@ -1,5 +1,6 @@
 import sqlite3
 import numpy as np
+from operator import itemgetter
 
 # connect to database
 conn = sqlite3.connect("FindingFunctions.db")
@@ -17,14 +18,51 @@ def get_list_of_column(column, table):
     return list_of_column
 
 # function for subtracting 2 lists
-def subtract_lists(list_1, list_2):
-    return np.array(list_1) - np.array(list_2)
+def sum_of_squared_deviations(list_1, list_2):
+    #not_squared = np.array(list_1) - np.array(list_2)
+    squared = np.square(np.array(list_1) - np.array(list_2))
+    sum_of_squared = np.sum(squared)
+    return sum_of_squared
+
+# calculating the sum of squared deviation + print for all the 50 functions 
+all_the_sums = []
+for i in range(1, 51):
+    for k in range(1, 5):
+        ideal = get_list_of_column(("y" + str(i)), "ideal")
+        train = get_list_of_column(("y" + str(k)), "train")
+        all_the_sums.append([("y" + str(i)), ("y" + str(k)), 
+        (sum_of_squared_deviations(ideal, train))])
+        
+sorted_list = sorted(all_the_sums, key=itemgetter(2))
+four_ideal = sorted_list[:4]
+#print(four_ideal)
+
+"""
+Result of the equation (ideal, train, sum)
+[['y31', 'y4', 31.128519434568638], 
+['y46', 'y1', 32.660363984619835], 
+['y6', 'y2', 33.74643722393511], 
+['y25', 'y3', 35.39435460328838]]
+"""
+
+# one ideal function for demonstration purposes iy31ty4
+ideal_y31 = get_list_of_column("y31", "ideal")
+train_y4 = get_list_of_column("y4", "train")
+
+
+
+def max_deviation(list_1, list_2):
+    # list of squared deviations
+    return np.max(np.square(np.array(list_1) - np.array(list_2)))
+
+#print(max_deviation(ideal_y31, train_y4))
+"""
+Result for the demonstration function and its dedicated train function
+0.24836179255280993
+"""
     
-# print(get_list_of_column("x", "train"))
-# all my columns
-ideal_y1 = get_list_of_column("y1", "ideal")
-train_y1 = get_list_of_column("y1", "train")
-print(subtract_lists(ideal_y1, train_y1))
+
+
 
 # commit and close connection to database    
 conn. commit()
