4405ac423ccb2b8370cc1c57a7d9b5337adb003f
Michael Dollinger <michi.dollinger96@web.de>
2021-11-16 11:29:01 +0100

    implemented oop for all the steps to find the ideal function

--- PythonProject/Function.py
+++ PythonProject/Function.py
@@ -1,6 +1,6 @@
 import sqlite3
 import numpy as np
-from operator import itemgetter
+#from operator import itemgetter
 
 # connect to database
 conn = sqlite3.connect("FindingFunctions.db")
@@ -33,17 +33,23 @@ class Ideal(Function):
         return sum_of_squared
 
     def max_deviation(self, train_column):
-        return np.max(np.square(np.array(self.get_column) - np.array(train_column.getcolumn())))
+        return np.max(np.square(np.array(self.get_column()) - np.array(train_column.get_column())))
 
 class Train(Function):
+
     def __init__(self, column):
         super().__init__(column, "train")
 
 
+class Result:
+
+    def __init__(self, ideal, train, deviation):
+        self.ideal = ideal
+        self.train = train
+        self.deviation = deviation
+
 y_2 = Ideal("y2")
 print(y_2.get_column())
-
-
 
 # calculating sum of sd for every train, ideal pair
 all_the_sums = []
@@ -51,70 +57,20 @@ for i in range(1, 51):
     for k in range(1, 5):
         ideal_column = Ideal("y" + str(i))
         train_column = Train("y" + str(k))
-        all_the_sums.append([("y" + str(i)), ("y" + str(k)), (ideal_column.sum_of_squared_deviations(train_column))]) 
+        res = Result("y" + str(i), "y" + str(k), ideal_column.sum_of_squared_deviations(train_column))
+        all_the_sums.append(res) 
 
-sorted_list = sorted(all_the_sums, key=itemgetter(2))
+# sort all deviations to get the 4 smallest
+sorted_list = sorted(all_the_sums, key=lambda x: x.deviation)
 four_ideal = sorted_list[:4]
-print(four_ideal)
-first_ideal_function = four_ideal[0]
-first_ideal = Ideal(first_ideal_function[0])
-print(first_ideal.get_column())
 
-first_train = Train(first_ideal_function[1])
-print(first_train.get_column())
-
-print(first_ideal.max_deviation(first_train))
+# print 4 ideal functions
+for i in four_ideal:
+    print(i.ideal)
+    print(i.train)
+    print(i.deviation)
 
 """
-#Result of the equation (ideal, train, sum)
-#[['y31', 'y4', 31.128519434568638], 
-#['y46', 'y1', 32.660363984619835], 
-#['y6', 'y2', 33.74643722393511], 
-#['y25', 'y3', 35.39435460328838]]
-"""
-
-
-
-
-
-
-
-"""
-
-# one ideal function for demonstration purposes iy31ty4
-ideal_y31 = get_list_of_column("y31", "ideal")
-train_y4 = get_list_of_column("y4", "train")
-train_y1 = get_list_of_column("y1", "train")
-train_y2 = get_list_of_column("y2", "train")
-train_y3 = get_list_of_column("y3", "train")
-test_x = get_list_of_column("x", "test")
-test_y = get_list_of_column("y", "test")
-ideal_y31 = get_list_of_column("y31", "ideal")
-ideal_y46 = get_list_of_column("y46", "ideal")
-ideal_y6 = get_list_of_column("y6", "ideal")
-ideal_y25 = get_list_of_column("y25", "ideal")
-
-
-def max_deviation(list_1, list_2):
-    # list of squared deviations
-    return np.max(np.square(np.array(list_1) - np.array(list_2)))
-
-max_dev_y31 = max_deviation(ideal_y31, train_y4)
-print(max_dev_y31)
-max_dev_y46 = max_deviation(ideal_y46, train_y1)
-print(max_dev_y46)
-max_dev_y6 = max_deviation(ideal_y6, train_y2)
-print(max_dev_y6)
-max_dev_y25 = max_deviation(ideal_y25, train_y3)
-print(max_dev_y25)
-
-#Result for the demonstration function and its dedicated train function
-#0.24836179255280993
-
-
-
-#print(test_x)
-#print(test_y)
 
 all_deviations = []
 deviation_2 = []
@@ -182,8 +138,8 @@ all_deviations.clear()
 #get_y_in_ideal("y25")
 
 
-"""
     
 # commit and close connection to database    
 conn. commit()
 conn.close()
+"""
