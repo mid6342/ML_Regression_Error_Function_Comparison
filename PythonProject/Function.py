33baad4bd4a2d24c7751c57f80d143c765a6a28d
Michael Dollinger <michi.dollinger96@web.de>
2021-11-16 10:04:08 +0100

    started implementation of oop

--- PythonProject/Function.py
+++ PythonProject/Function.py
@@ -6,44 +6,79 @@ from operator import itemgetter
 conn = sqlite3.connect("FindingFunctions.db")
 cur = conn.cursor()
 
-# function to get a whole column in form of a list
-def get_list_of_column(column, table):
-    # get all the cells of the column and write them to a list
-    query= cur.execute("SELECT " + column + " FROM " + table)
-    list_of_column = []
-    for cell in query: 
-        list_of_column.append(cell[0])  
-    # remove column head
-    list_of_column.pop(0)
-    return list_of_column
+class Function:
+
+    def __init__(self, column, table):
+        self.column = column
+        self.table = table
+
+    def get_column(self):
+        # get all the cells of the column and write them to a list
+        query= cur.execute("SELECT " + self.column + " FROM " + self.table)
+        column_list = []
+        for cell in query: 
+            column_list.append(cell[0])  
+        # remove column head
+        column_list.pop(0)
+        return column_list
+
+class Ideal(Function):
+
+    def __init__(self, column):
+        super().__init__(column, "ideal")
+
+    def sum_of_squared_deviations(self, train_column):
+        squared = np.square(np.array(self.get_column()) - np.array(train_column.get_column()))
+        sum_of_squared = np.sum(squared)
+        return sum_of_squared
+
+    def max_deviation(self, train_column):
+        return np.max(np.square(np.array(self.get_column) - np.array(train_column.getcolumn())))
+
+class Train(Function):
+    def __init__(self, column):
+        super().__init__(column, "train")
 
 
-# function for subtracting 2 lists
-def sum_of_squared_deviations(list_1, list_2):
-    #not_squared = np.array(list_1) - np.array(list_2)
-    squared = np.square(np.array(list_1) - np.array(list_2))
-    sum_of_squared = np.sum(squared)
-    return sum_of_squared
+y_2 = Ideal("y2")
+print(y_2.get_column())
 
-# calculating the sum of squared deviation + print for all the 50 functions 
+
+
+# calculating sum of sd for every train, ideal pair
 all_the_sums = []
 for i in range(1, 51):
     for k in range(1, 5):
-        ideal = get_list_of_column(("y" + str(i)), "ideal")
-        train = get_list_of_column(("y" + str(k)), "train")
-        all_the_sums.append([("y" + str(i)), ("y" + str(k)), 
-        (sum_of_squared_deviations(ideal, train))])
-        
+        ideal_column = Ideal("y" + str(i))
+        train_column = Train("y" + str(k))
+        all_the_sums.append([("y" + str(i)), ("y" + str(k)), (ideal_column.sum_of_squared_deviations(train_column))]) 
+
 sorted_list = sorted(all_the_sums, key=itemgetter(2))
 four_ideal = sorted_list[:4]
 print(four_ideal)
+first_ideal_function = four_ideal[0]
+first_ideal = Ideal(first_ideal_function[0])
+print(first_ideal.get_column())
+
+first_train = Train(first_ideal_function[1])
+print(first_train.get_column())
+
+print(first_ideal.max_deviation(first_train))
 
 """
-Result of the equation (ideal, train, sum)
-[['y31', 'y4', 31.128519434568638], 
-['y46', 'y1', 32.660363984619835], 
-['y6', 'y2', 33.74643722393511], 
-['y25', 'y3', 35.39435460328838]]
+#Result of the equation (ideal, train, sum)
+#[['y31', 'y4', 31.128519434568638], 
+#['y46', 'y1', 32.660363984619835], 
+#['y6', 'y2', 33.74643722393511], 
+#['y25', 'y3', 35.39435460328838]]
+"""
+
+
+
+
+
+
+
 """
 
 # one ideal function for demonstration purposes iy31ty4
@@ -59,8 +94,6 @@ ideal_y46 = get_list_of_column("y46", "ideal")
 ideal_y6 = get_list_of_column("y6", "ideal")
 ideal_y25 = get_list_of_column("y25", "ideal")
 
-print("This is ideal 25")
-print(ideal_y25)
 
 def max_deviation(list_1, list_2):
     # list of squared deviations
@@ -74,10 +107,9 @@ max_dev_y6 = max_deviation(ideal_y6, train_y2)
 print(max_dev_y6)
 max_dev_y25 = max_deviation(ideal_y25, train_y3)
 print(max_dev_y25)
-"""
-Result for the demonstration function and its dedicated train function
-0.24836179255280993
-"""
+
+#Result for the demonstration function and its dedicated train function
+#0.24836179255280993
 
 
 
@@ -150,7 +182,7 @@ all_deviations.clear()
 #get_y_in_ideal("y25")
 
 
-
+"""
     
 # commit and close connection to database    
 conn. commit()
