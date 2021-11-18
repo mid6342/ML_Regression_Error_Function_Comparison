f5316ed8f555d4f001f7618510e8af83138ad6ad
Michael Dollinger <michi.dollinger96@web.de>
2021-11-18 12:15:29 +0100

    implemented criterion2 and printed the results

--- PythonProject/Function.py
+++ PythonProject/Function.py
@@ -57,8 +57,17 @@ class Result:
         self.train = train
         self.deviation = deviation
 
+class FunctionAndMaxDev:
+
+    def __init__(self, ideal, deviation, max_deviation, criterion_2):
+        self.ideal = ideal
+        self.deviation = deviation
+        self.max_deviation = max_deviation
+        self.criterion_2 = criterion_2
+
 y_2 = Ideal("y2")
-print(y_2.get_column())
+#print(y_2.get_column())
+#print(Train("y3"))
 
 # calculating sum of sd for every train, ideal pair
 all_the_sums = []
@@ -75,15 +84,17 @@ four_ideal = sorted_list[:4]
 
 x = Test("x")
 x_column = x.get_column()
-print(x_column)
+#print(x_column)
 
-# print 4 ideal functions
 
+four_deviations = []
+
+# for loop to get all the deviations between each test point and each ideal function, now criterion
 for i in four_ideal:
     
     print(i.ideal)
     print(i.train)
-    print(i.deviation)
+    #print(i.deviation)
 
     first_time = True
     all_deviations = []
@@ -110,9 +121,43 @@ for i in four_ideal:
             deviation = abs(y_test[1] - y_ideal)
 
         y_test.clear()
-        all_deviations.append(deviation[0])
+        all_deviations.append(deviation[0]) # 0 nuir um die Ergebnisse im Arraay zhu nem array und keinem tuopel zu machen
 
-    print(all_deviations)
+    # calculate max deviation between the respective train and ideal function
+    y = Ideal(i.ideal)
+    f = Train(i.train)
+    max_deviation = y.max_deviation(f)
+    #four_deviations.append(all_deviations)   
+    res = FunctionAndMaxDev(i.ideal, all_deviations, max_deviation, "None")
+    four_deviations.append(res)
+
+print(four_deviations)
+    #res = Result(i.ideal, i.train, all_deviations)
+    #four_deviations.append(res)
+
+
+criterion_2_array = []
+for i in four_deviations:   
+    #print(i.deviation)
+    print(i.ideal)
+    #print(i.max_deviation)
+    #print("Hello:  " + str(i.criterion_2))
+
+    for x in i.deviation:
+        if(x <= i.max_deviation + np.sqrt(2)):
+            #print("Criterion2 = " + str((i.max_deviation + np.sqrt(2)) - x))
+            z = i.max_deviation + np.sqrt(2) - x
+            np.array(criterion_2_array.append(z))
+        else:
+            y = "None"
+            np.array(criterion_2_array.append(y))
+    print(criterion_2_array)
+    criterion_2_array.clear()
+            
+    
+
+
+
        
 # commit and close connection to database    
 conn. commit()
