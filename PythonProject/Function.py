3b544b1b77aba39ce539345d1c82e9a5184798f7
Michael Dollinger <michi.dollinger96@web.de>
2021-11-19 18:31:57 +0100

    encapsulated the functions for criterion 1

--- PythonProject/Function.py
+++ PythonProject/Function.py
@@ -3,11 +3,17 @@ import numpy as np
 #from operator import itemgetter
 from bokeh.plotting import figure, show, output_file
 
+    
 # connect to database
 conn = sqlite3.connect("FindingFunctions.db")
 cur = conn.cursor()
 
+"""
 
+CLASSES
+
+
+"""
 class Function:
 
     def __init__(self, column, table):
@@ -24,16 +30,15 @@ class Function:
         column_list.pop(0)
         return column_list
 
-
 class Ideal(Function):
 
-    def __init__(self, column):
+    def __init__(self, column, sum_of_squared_deviations = None):
         super().__init__(column, "ideal")
+        self.sum_of_squared_deviations = sum_of_squared_deviations
 
-    def sum_of_squared_deviations(self, train_column):
+    def get_median_deviation(self, train_column):
         squared = np.square(np.array(self.get_column()) - np.array(train_column.get_column()))
-        sum_of_squared = np.sum(squared)
-        return sum_of_squared
+        return np.sum(squared)
         
     def max_deviation(self, train_column):
         return np.max(np.square(np.array(self.get_column()) - np.array(train_column.get_column())))
@@ -45,18 +50,17 @@ class Train(Function):
         super().__init__(column, "train")
 
 
+class MeanDeviation:
+
+    def __init__(self, ideal, train, median_deviation):
+        self.ideal = ideal
+        self.train = train
+        self.median_deviation = median_deviation
+
 class Test(Function):
 
     def __init__(self, column):
         super().__init__(column, "test")
-
-
-class Result:
-
-    def __init__(self, ideal, train, deviation):
-        self.ideal = ideal
-        self.train = train
-        self.deviation = deviation
 
 class FunctionAndMaxDev:
 
@@ -73,35 +77,51 @@ CRITERION 1.
 
 """
 
-# calculating sum of sd for every train, ideal pair
+def calculate_four_ideal_functions():
+    all_deviations = []
 
-#def calculate_4_ideal_functions():
-all_the_sums = []
-for ideal_index in range(1, 51):
+    # calculating sum of sd for every train, ideal pair
+    for ideal_index in range(1, 51):
+        for train_index in range(1, 5):    
 
-    for train_index in range(1, 5):
-            
-        ideal_column = Ideal("y" + str(ideal_index))
-        train_column = Train("y" + str(train_index))
-        res = Result("y" + str(ideal_index), "y" + str(train_index), 
-        ideal_column.sum_of_squared_deviations(train_column))
-        all_the_sums.append(res) 
+            ideal_fnc = Ideal("y" + str(ideal_index))
+            train_fnc = Train("y" + str(train_index))
 
-# sort all deviations to get the 4 smallest
-sorted_list = sorted(all_the_sums, key=lambda x: x.deviation)
-four_ideal = sorted_list[:4]
+            res = MeanDeviation(ideal_fnc.column, train_fnc.column, 
+            ideal_fnc.get_median_deviation(train_fnc))
+
+            all_deviations.append(res) 
+
+    # sort all median deviations to get the 4 ideal functions with the smallest median deviation
+    sorted_deviations = sorted(all_deviations, key=lambda x: x.median_deviation)
+    return sorted_deviations[:4]
+
+def visualize_ideal_and_train(four_ideal_fnc, x):
+
+    for i in four_ideal_fnc:
+
+        p = figure(
+            title = 'ideal_and_train',
+            x_axis_label = 'X',
+            y_axis_label = 'Y',
+        )
+
+        # get the right columns
+        y_ideal = Ideal(i.ideal)
+        y_ideal = y_ideal.get_column()
+        y_train = Train(i.train)
+        y_train = y_train.get_column()
+
+        p.line(x, y_ideal, line_color='red', line_width = 1.25)
+        p.line(x, y_train, line_width = 1.25)
+        show(p)
 
 """
 
-CRITERION 2.
+CRITERION 2. VISUALIZATION
 
 
 """
-
-hallox = Test("x")
-x_column = hallox.get_column()
-halloy = Test("y")
-y_column = halloy.get_column()
 
 #output_file = 'index.html'
 #evtl max y und max x von den testpoints verwenden
@@ -117,7 +137,14 @@ def visualize(x, y, x_1, y_1, x_error_band, y_error_band, y_error_band_minus):
     p.circle(x_column, y_column, size =5, color = 'green')
     p.line(x_error_band, y_error_band, line_color='orange', line_width = 1.25)
     p.line(x_error_band, y_error_band_minus, line_color='orange', line_width = 1.25)
-    show(p)
+    show(p)   
+
+hallox = Test("x")
+x_column = hallox.get_column()
+halloy = Test("y")
+y_column = halloy.get_column()
+
+
 
 #plotting an ideal function
 x_ideal = Ideal("x")
@@ -128,7 +155,7 @@ f_y_column = []
 y_error_band = []
 y_error_band_minus = []
 k = 0
-for i in four_ideal:
+for i in calculate_four_ideal_functions():
     #print(i.ideal)
     y = Ideal(i.ideal)
     y_ideal_column.append(y.get_column()) 
@@ -144,8 +171,15 @@ for i in four_ideal:
 
 four_deviations = []
 
+"""
+
+CRITERION 2. CODE
+
+
+"""
+
 # for loop to get all the deviations between each test point and each ideal function, now criterion
-for i in four_ideal:
+for i in calculate_four_ideal_functions():
     
     #print(i.ideal)
     #print(i.train)
@@ -243,8 +277,26 @@ for i in all:
 #for i in result:
 #    print(i)
 
-       
+    
+
+
+
+
+def main():
+    
+    x = Function("x", "train")
+    x = x.get_column()
+    visualize_ideal_and_train(calculate_four_ideal_functions(), x)
+
+    print("the median_deviation")
+    for i in calculate_four_ideal_functions():
+        print(i.median_deviation)
+
+
+if __name__ == '__main__':
+    main()
+
+
 # commit and close connection to database    
 conn. commit()
 conn.close()
-
